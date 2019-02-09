b662108a54a22fdd8089fff9e16c7c5a4b5a2e6f
Acacia Pappas <acacia.m.pappas@gmail.com>
2019-02-08 19:14:33 -0800

    "Add GET from server to db"

--- client/src/index.js
+++ client/src/index.js
@@ -1,20 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// class Photos extends React.Component {
-//   constructor(props){
-//     super(props)
-
-//   }
-
-//   render(){
-//     return 
-    
-      
-    
-//   }
-// }
-
 ReactDOM.render(
   <div>Hello World</div>, 
   document.getElementById('app')
