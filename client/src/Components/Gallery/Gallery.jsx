08175404f2b348c0d7b3b0303956beaba3692b4e
Acacia Pappas <acacia.m.pappas@gmail.com>
2019-02-15 13:43:10 -0800

    Render modal content on click

--- client/src/Components/Gallery/Gallery.jsx
+++ client/src/Components/Gallery/Gallery.jsx
@@ -1,111 +1,118 @@
 import React from 'react';
 import Styled from './GalleryStyles.js';
+import Modal from '../Modal/Modal.jsx'
 
 class Gallery extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      showModal: false,
-      currentImg: {}
+      currentImg: {},
+      isOpen: false
     };
-
-    this.clickHandler = this.clickHandler.bind(this);
+    this.toggleModal = this.toggleModal.bind(this);
   }
 
-  clickHandler(e) {
-    var newState = {
-      showModal: true,
-      currentImg: e.target.value
-    };
-    this.setState(newState);
-
-    console.log('click', this.state.currentImg, 'modal:', this.state.showModal)
+  toggleModal(e) {
+    this.setState({
+      currentImg: e.target.value,
+      isOpen: !this.state.isOpen
+    });
   }
 
   render() {
-    if(this.props.data.length !== 0){
+    console.log('state', this.state)
+    if (this.props.data.length !== 0) {
       return (
-        <Styled.Gallery>
-          <Styled.Layout>
-  
-            <Styled.Item1>
-              <Styled.Button value={this.props.data[0]} onClick={this.clickHandler}>
-                <Styled.PhotoContainer>
-                  <Styled.Photo src={this.props.data[0].url}></Styled.Photo>
-                </Styled.PhotoContainer>
-              </Styled.Button>
-            </Styled.Item1>
+        <div>
 
-            <Styled.Item2>
-              <Styled.Button value={this.props.data[1].url} onClick={this.clickHandler}>
-                <Styled.PhotoContainer>
-                  <Styled.Photo src={this.props.data[1].url}></Styled.Photo>
-                </Styled.PhotoContainer>
-              </Styled.Button>
-            </Styled.Item2>
-  
-            <Styled.Item3>
-              <Styled.Button value={this.props.data[2].url} onClick={this.clickHandler}>
-                <Styled.PhotoContainer>
-                  <Styled.Photo src={this.props.data[2].url}></Styled.Photo>
-                </Styled.PhotoContainer>
-              </Styled.Button>
-            </Styled.Item3>
-  
-            <Styled.Item4>
-              <Styled.Button>
-                <Styled.PhotoContainer>
-                  <Styled.Photo src={this.props.data[3].url} ></Styled.Photo>
-                </Styled.PhotoContainer>
-              </Styled.Button>
-            </Styled.Item4>
-  
-            <Styled.Item5>
-              <Styled.Button>
-                <Styled.PhotoContainer>
-                  <Styled.Photo src={this.props.data[4].url} ></Styled.Photo>
-                </Styled.PhotoContainer>
-              </Styled.Button>
-            </Styled.Item5>
-  
-            <Styled.Item6>
-              <Styled.Button>
-                <Styled.PhotoContainer>
-                  <Styled.Photo src={this.props.data[5].url} ></Styled.Photo>
-                </Styled.PhotoContainer>
-              </Styled.Button>
-            </Styled.Item6>
-  
-            <Styled.Item7>
-              <Styled.Button>
-                <Styled.PhotoContainer>
-                  <Styled.Photo src={this.props.data[6].url} ></Styled.Photo>
-                </Styled.PhotoContainer>
-              </Styled.Button>
-            </Styled.Item7>
-  
-            <Styled.Item8>
-              <Styled.Button>
-                <Styled.PhotoContainer>
-                  <Styled.Photo src={this.props.data[7].url} ></Styled.Photo>
-                </Styled.PhotoContainer>
-              </Styled.Button>
-            </Styled.Item8>
-  
-            <Styled.ItemLast>
-              <Styled.Button>
-                <Styled.PhotoContainer>
-                  <Styled.Photo 
-                  src={this.props.data[8].url} ></Styled.Photo>
+          <Styled.Gallery>
+            <Styled.Layout>
 
-                  <Styled.LastItemOverlay>+ {this.props.imgTotal - 9} more</Styled.LastItemOverlay>
-                  
-                </Styled.PhotoContainer>
-              </Styled.Button>
-            </Styled.ItemLast>
-  
-          </Styled.Layout>
-        </Styled.Gallery>
+              <Styled.Item1>
+                <Styled.Button value={this.props.data[0]} onClick={this.toggleModal}>
+                  <Styled.PhotoContainer>
+                    <Styled.Photo src={this.props.data[0].url}></Styled.Photo>
+                  </Styled.PhotoContainer>
+                </Styled.Button>
+              </Styled.Item1>
+
+              <Styled.Item2>
+                <Styled.Button value={this.props.data[1].url} onClick={this.toggleModal}>
+                  <Styled.PhotoContainer>
+                    <Styled.Photo src={this.props.data[1].url}></Styled.Photo>
+                  </Styled.PhotoContainer>
+                </Styled.Button>
+              </Styled.Item2>
+
+              <Styled.Item3>
+                <Styled.Button value={this.props.data[2].url} onClick={this.toggleModal}>
+                  <Styled.PhotoContainer>
+                    <Styled.Photo src={this.props.data[2].url}></Styled.Photo>
+                  </Styled.PhotoContainer>
+                </Styled.Button>
+              </Styled.Item3>
+
+              <Styled.Item4>
+                <Styled.Button value={this.props.data[3].url} onClick={this.toggleModal}>
+                  <Styled.PhotoContainer>
+                    <Styled.Photo src={this.props.data[3].url} ></Styled.Photo>
+                  </Styled.PhotoContainer>
+                </Styled.Button>
+              </Styled.Item4>
+
+              <Styled.Item5>
+                <Styled.Button value={this.props.data[4].url} onClick={this.toggleModal}>
+                  <Styled.PhotoContainer>
+                    <Styled.Photo src={this.props.data[4].url} ></Styled.Photo>
+                  </Styled.PhotoContainer>
+                </Styled.Button>
+              </Styled.Item5>
+
+              <Styled.Item6>
+                <Styled.Button value={this.props.data[5].url} onClick={this.toggleModal}>
+                  <Styled.PhotoContainer>
+                    <Styled.Photo src={this.props.data[5].url} ></Styled.Photo>
+                  </Styled.PhotoContainer>
+                </Styled.Button>
+              </Styled.Item6>
+
+              <Styled.Item7>
+                <Styled.Button value={this.props.data[6].url} onClick={this.toggleModal}>
+                  <Styled.PhotoContainer>
+                    <Styled.Photo src={this.props.data[6].url} ></Styled.Photo>
+                  </Styled.PhotoContainer>
+                </Styled.Button>
+              </Styled.Item7>
+
+              <Styled.Item8>
+                <Styled.Button value={this.props.data[7].url} onClick={this.toggleModal}>
+                  <Styled.PhotoContainer>
+                    <Styled.Photo src={this.props.data[7].url} ></Styled.Photo>
+                  </Styled.PhotoContainer>
+                </Styled.Button>
+              </Styled.Item8>
+
+              <Styled.ItemLast>
+                <Styled.Button value={this.props.data[8].url} onClick={this.toggleModal}>
+                  <Styled.PhotoContainer>
+                    <Styled.Photo
+                      src={this.props.data[8].url} ></Styled.Photo>
+
+                    <Styled.LastItemOverlay>+ {this.props.imgTotal - 9} more</Styled.LastItemOverlay>
+
+                  </Styled.PhotoContainer>
+                </Styled.Button>
+              </Styled.ItemLast>
+
+            </Styled.Layout>
+          </Styled.Gallery>
+
+          <Modal show={this.state.isOpen}
+            onClose={this.toggleModal}>
+            This is my Modal Content
+          </Modal>
+          
+        </div>
       )
     } else {
       return <div></div>
--- client/src/Components/Modal/ModalStyles.js
+++ client/src/Components/Modal/ModalStyles.js
@@ -1,30 +1,33 @@
 import styled from 'styled-components';
 
-const Overlay = styled.main`
-display: 'flex';
-alignItems: 'center';
-justifyContent: 'center';
-position: 'fixed';
-top: 0;
-right: 0;
-left: 0;
-bottom: 0;
-padding: '1rem';
-backgroundColor: 'rgba(0, 0, 0, 0.7)';
-zIndex: '9999';
-opacity: 1;
-animation: 'show .5s ease';
-overflowX: 'hidden';
-overflowY: 'auto';
+const BackDropStyle = styled.div`
+display: b,
+alignItems: center,
+justifyContent: center,
+position: fixed,
+top: 0,
+right: 0,
+left: 0,
+bottom: 0,
+padding: 1rem,
+background-color: rgba(0, 0, 0, 0.7),
+z-index: 9999,
+animation: show .5s ease,
+overflow: hidden,
 `;
 
-const Exit = styled.a`
-
+const ModalStyle = styled.div`
+background-color: '#fff',
+borde-radius: 5,
+max-width: 500,
+min-height: 300,
+margin: '0 auto',
+padding: 30
 `;
 
 const PhotoContent = styled.div`
-
+  background-color: black;
 `;
 
 
-export default {Overlay, Exit, PhotoContent};
+export default {BackDropStyle, ModalStyle, PhotoContent};
