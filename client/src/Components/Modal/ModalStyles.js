599f159229361de143cf3b0a6d6e8126391a4032
Acacia Pappas <acacia.m.pappas@gmail.com>
2019-02-15 10:37:58 -0800

    Add click handler for all images

--- client/src/Components/Modal/ModalStyles.js
+++ client/src/Components/Modal/ModalStyles.js
@@ -1,7 +1,21 @@
 import styled from 'styled-components';
 
 const Overlay = styled.main`
-
+display: 'flex';
+alignItems: 'center';
+justifyContent: 'center';
+position: 'fixed';
+top: 0;
+right: 0;
+left: 0;
+bottom: 0;
+padding: '1rem';
+backgroundColor: 'rgba(0, 0, 0, 0.7)';
+zIndex: '9999';
+opacity: 1;
+animation: 'show .5s ease';
+overflowX: 'hidden';
+overflowY: 'auto';
 `;
 
 const Exit = styled.a`
@@ -11,5 +25,6 @@ const Exit = styled.a`
 const PhotoContent = styled.div`
 
 `;
+
 
 export default {Overlay, Exit, PhotoContent};
