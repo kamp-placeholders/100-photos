94cc93ff24754188546e49ea88d32c6403763488
Acacia Pappas <acacia.m.pappas@gmail.com>
2019-02-15 14:54:02 -0800

    Render modal background using react Portal

--- client/src/Components/Modal/Modal.jsx
+++ client/src/Components/Modal/Modal.jsx
@@ -1,28 +1,47 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import Styled from './ModalStyles.js';
+
+const appRoot = document.getElementById('app');
+const modalRoot = document.getElementById('modal');
 
 
 class Modal extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+
+    }
+    this.el = document.createElement('div');
+  }
+
+  componentDidMount() {
+    modalRoot.appendChild(this.el);
+  }
+
+  componentWillUnmount() {
+    modalRoot.removeChild(this.el);
+  }
+
   render() {
     // Render nothing if the "show" prop is false
     if(!this.props.show) {
       return null;
     }
-
-    return (
+    
+    return ReactDOM.createPortal(
       <Styled.BackDropStyle>
         <Styled.ModalStyle >
           {this.props.children}
           <div className="footer">
-            <button onClick={this.props.onClose}>
-              Close
-            </button>
           </div>
         </Styled.ModalStyle >
-      </Styled.BackDropStyle>
-    );
+      </Styled.BackDropStyle>,
+      this.el
+      );
+    }
   }
-}
-
-export default Modal;
-
+  
+  export default Modal;
+  
+  
