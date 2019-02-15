e46356ed0eb906e3b8b15731b9496ff1ebd66207
Acacia Pappas <acacia.m.pappas@gmail.com>
2019-02-15 15:42:32 -0800

    Add Modal contents as Carousel class component

---
+++ client/src/Components/Carousel/Carousel.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class Carousel extends React.Component {
+  render() 
+  {
+    return <button onClick={props.toggle}>click</button>
+  }
+}
+
+export default Carousel;
