7fd5f5450239357bca3f0b3547e8b6e39585e5d7
Acacia Pappas <acacia.m.pappas@gmail.com>
2019-02-16 12:48:54 -0800

    Center Carousel content and add Right and Left Buttons

--- client/src/Components/Carousel/CarouselStyles.js
+++ client/src/Components/Carousel/CarouselStyles.js
@@ -32,9 +32,94 @@ const Exit = styled.button`
   :after {
     content: 'X'; 
   }
-  `;
+`;
   
+const Main = styled.div`
+  display: grid; 
+  grid-template-columns: 20% 60% 20%;
+  border: solid white 1px;
+  width: 660px;
+  min-height: 300px;
+  min-height: 300px;
+  justify-self: center;
+  justify-content: center;
+  position: relative;
+`;
+
+const Content = styled.div`
+border: solid green 2px;
+position: absolute;
+display: inline; 
+grid-area: auto / 2 / auto / span 1
+justify-content: center; 
+
+  width: 400px;
+  height: auto;
+  max-height: 590;
+  max-width: 590;
+  min-height: 300px;
+  min-height: 300px;
+`;
+
+const LeftButton = styled.a`
+  left: 0;
+  color: #91949a;
+  font-size: 1.2rem;
+  position: absolute;
+  top: 50%;
+  left: 0;
+  font-family: icons;
+  speak: none;
+  font-style: normal;
+  font-weight: 700;
+  font-variant: normal;
+  text-transform: none;
+  line-height: 1;
+  content: ">";
+`;
+
+const RightButton = styled.a`
+  right: 0;
+  color: white; 
+  font-size: 1.2rem;
+  position: absolute;
+  top: 50%;
+  font-family: icons;
+  speak: none;
+  font-style: normal;
+  font-weight: 700;
+  font-variant: normal;
+  text-transform: none;
+  line-height: 1;
+  -webkit-font-smoothing: antialiased;
+  content: "<";
+`;
+
+const PhotoContainer = styled.div`
+border: solid pink 1px;
+
+display: inline; 
+  width: 300px;
+  height: 300px
+  max-height: 590;
+  max-width: 590;
+  min-height: 200px;
+  min-height: 200px;
+`;
+
+const Photo = styled.img`
+border: solid red 1px;
+  justify-content: center;
+  overflow: hidden;
+  width: 100%;
+  height: 100%;
+  background-size: cover; 
+  position: relative; 
+`;
+
+const Description = styled.div`
+  color: white; 
+`;
 
 
-
-export default {Exit}
+export default {Exit, Content, Main, Photo, PhotoContainer, Description, LeftButton, RightButton}
