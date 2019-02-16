1fd62c7cd9895967054eb763984ed520b09f527c
Acacia Pappas <acacia.m.pappas@gmail.com>
2019-02-16 12:48:54 -0800

    Center Carousel content and add Right and Left Buttons

--- client/src/Components/Carousel/Carousel.jsx
+++ client/src/Components/Carousel/Carousel.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
 import Styled from './CarouselStyles.js'
 
+
 class Carousel extends React.Component {
   render() 
   {
-    return <Styled.Exit onClick={this.props.toggle}></Styled.Exit>
+    let i = 0;
+    return (
+      <div>
+        <Styled.Main>
+                <Styled.LeftButton>click</Styled.LeftButton>
+            <Styled.Content>
+
+                  <Styled.PhotoContainer>
+                    <Styled.Photo src={this.props.data[i].url}></Styled.Photo>
+                  </Styled.PhotoContainer>
+          
+                  <Styled.Description>{this.props.data[i].description}</Styled.Description>
+
+            </Styled.Content>
+                <Styled.RightButton>Click</Styled.RightButton>
+
+        </Styled.Main>
+        <Styled.Exit onClick={this.props.toggle}></Styled.Exit>
+      </div>
+    )
   }
 }
 
