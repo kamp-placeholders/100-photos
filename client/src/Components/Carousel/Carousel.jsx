1f0916e87ae09c2301137fa8892347f0c149c15f
Acacia Pappas <acacia.m.pappas@gmail.com>
2019-02-16 23:47:14 -0800

    Add fn to track current photo index in state

--- client/src/Components/Carousel/Carousel.jsx
+++ client/src/Components/Carousel/Carousel.jsx
@@ -6,38 +6,75 @@ class Carousel extends React.Component {
   constructor(props){
     super(props)
     this.state = {
-
+      photos: this.props.data, 
+      currentId: Number(this.props.current),
+      currentIndex: 0,
+      currentPhoto: {}
     }
+
+    this.leftClickHandler = this.leftClickHandler.bind(this);
+    this.rightClickHandler = this.rightClickHandler.bind(this);
+  }
+
+  componentDidMount() {
+    let allPhotos = this.state.photos; //array
+
+    //find the current photo by the ID and display that. 
+    //also, find the index of it and store it in state for the clickers
+
+    let foundIndex = 0; 
+    let foundCurrentPhoto = allPhotos.filter((item) => {
+      if(item.id === this.state.currentId) {
+        foundIndex = this.props.data.findIndex((e) => e.id === item.id);
+      }
+      return item.id === this.state.currentId; 
+    });
+
+  let newState = {
+    currentIndex: foundIndex,
+    currentPhoto: foundCurrentPhoto
+  }
+    this.setState(newState);
   }
   
+  leftClickHandler(e) {
+    console.log('left click')
+  }
+
+  rightClickHandler(e) {
+    console.log('right click')
+
+  }
+
   render() 
   {
-    console.log(this.props.data[0]);
-    let i = 0;
+    console.log('state', this.state);
+    const i = this.state.index;
     return (
       <div>
         <Styled.Main>
-                <Styled.LeftButton/>
+                <Styled.LeftButton onClick={this.leftClickHandler}/>
             <Styled.Content>
 
                   <Styled.PhotoContainer>
-                    <Styled.Photo src={this.props.data[i].url}></Styled.Photo>
+                    <Styled.Photo src={this.props.data[this.state.currentIndex].url}></Styled.Photo>
                   </Styled.PhotoContainer>
           
                   <Styled.Description>
                     <Styled.Text>
                       <div>
-                        <Styled.Strong>{this.props.data[i].description}</Styled.Strong>{this.props.data[i].date}
+                        <Styled.Strong>{this.props.data[this.state.currentIndex].description}</Styled.Strong>{this.props.data[this.state.currentIndex].date}
                       </div>
                       <div>
-                        {this.props.data[i].source}
+                        {this.props.data[this.state.currentIndex].source}
                       </div>
+                      <Styled.Flag src='/Users/acaciapappas/Desktop/placeholders/100-photos/client/dist/flag.png'/>
                     </Styled.Text>
                   
                   </Styled.Description>
 
             </Styled.Content>
-                <Styled.RightButton/>
+                <Styled.RightButton onClick={this.rightClickHandler}/>
 
         </Styled.Main>
         <Styled.Exit onClick={this.props.toggle}></Styled.Exit>
