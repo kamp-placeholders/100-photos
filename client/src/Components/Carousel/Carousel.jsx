6272d6b32d2f65c59b7842fdc2eab30124dae14e
Acacia Pappas <acacia.m.pappas@gmail.com>
2019-02-16 17:54:52 -0800

    Add styles to carousel photo description

--- client/src/Components/Carousel/Carousel.jsx
+++ client/src/Components/Carousel/Carousel.jsx
@@ -3,23 +3,41 @@ import Styled from './CarouselStyles.js'
 
 
 class Carousel extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+
+    }
+  }
+  
   render() 
   {
+    console.log(this.props.data[0]);
     let i = 0;
     return (
       <div>
         <Styled.Main>
-                <Styled.LeftButton>click</Styled.LeftButton>
+                <Styled.LeftButton/>
             <Styled.Content>
 
                   <Styled.PhotoContainer>
                     <Styled.Photo src={this.props.data[i].url}></Styled.Photo>
                   </Styled.PhotoContainer>
           
-                  <Styled.Description>{this.props.data[i].description}</Styled.Description>
+                  <Styled.Description>
+                    <Styled.Text>
+                      <div>
+                        <Styled.Strong>{this.props.data[i].description}</Styled.Strong>{this.props.data[i].date}
+                      </div>
+                      <div>
+                        {this.props.data[i].source}
+                      </div>
+                    </Styled.Text>
+                  
+                  </Styled.Description>
 
             </Styled.Content>
-                <Styled.RightButton>Click</Styled.RightButton>
+                <Styled.RightButton/>
 
         </Styled.Main>
         <Styled.Exit onClick={this.props.toggle}></Styled.Exit>
