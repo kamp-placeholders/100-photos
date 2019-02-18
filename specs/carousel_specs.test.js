f3411408597050cff7ece50c5b725ab0424a9bcd
Acacia Pappas <acacia.m.pappas@gmail.com>
2019-02-18 10:45:31 -0800

    Add tests for Carousel component

--- specs/carousel_specs.test.js
+++ specs/carousel_specs.test.js
@@ -2,22 +2,38 @@ import React from 'react';
 import Enzyme, {shallow, mount} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import Carousel from '../client/src/Components/Carousel/Carousel.jsx';
+import dummyData from '../client/src/sampleData.js';
 
 Enzyme.configure({ adapter: new Adapter() });
 
 describe("Carousel Component", () => {
+  const wrapperMount = mount(<Carousel/>);
+  const wrapperShallow = shallow(<Carousel/>);
+  wrapperMount.setProps = {
+    data: dummyData
+  }
   test("renders", () => {
-    const wrapper = shallow(<Carousel/>);
-    expect(wrapper.exists()).toBe(true);
+    expect(wrapperShallow.exists()).toBe(true);
   })
   test("match snapshot", () => {
-    const wrapper = mount(<Carousel/>);
-    expect(wrapper).toMatchSnapshot()
+    expect(wrapperMount).toMatchSnapshot()
   })
-  test("an exit button exists", () => {
-    const wrapper = mount(<Carousel/>);
+  test("an exit button exists and works", () => {
     expect(
-      wrap.find('button').text()
-    ).toEqual('X')
+      wrapperMount.find('button').text()
+    ).toEqual('X').simulate('click');
+  })
+  test("right and left buttons work", () => {
+    expect(
+      wrapperMount.find('button').toEqual('<').simulate('click')
+      );
+    expect(
+      wrapperMount.find('button').toEqual('>').simulate('click')
+    );
+  })
+  test("contains image element", () => {
+    expect(
+      wrapperMount.find('img').hasClass('CarouselStyles__Photo-ocec9w-6 hBPtId')
+    )
   })
 });
--- specs/gallery_specs.test.js
+++ specs/gallery_specs.test.js
@@ -2,12 +2,22 @@ import React from 'react';
 import Enzyme, {shallow, mount} from 'enzyme';
 import Gallery from '../client/src/Components/Gallery/Gallery';
 import Adapter from 'enzyme-adapter-react-16';
+import dummyData from '../client/src/sampleData.js';
 
 Enzyme.configure({ adapter: new Adapter() });
 
 describe("Gallery component", () => {
-  const wrapper = mount(<Gallery/>);
+  const wrapperMount = mount(<Gallery/>);
+  wrapperMount.setProps = {
+    data: dummyData
+  }
   test("renders", () => {
-      expect(wrapper.exists()).toBe(true);
+      expect(wrapperMount.exists()).toBe(true);
     })
+  test("snapshot", () => {
+    expect(wrapperMount).toMatchSnapshot()
+  });
+  test("images are buttons", () => {
+    wrapperMount.find('a').simulate('click');
+  })
   });
