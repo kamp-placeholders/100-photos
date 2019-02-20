e689bed72ae0ff4005b58884612329a6b8cd873c
Acacia Pappas <acacia.m.pappas@gmail.com>
2019-02-20 08:45:29 -0800

    Refactor styled-components to cdn for successful integration

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './Components/App/App.jsx';
 
-ReactDOM.render(<App />, document.getElementById('100-Photos'));
+ReactDOM.render(<App />, document.getElementById('photos'));
 
 
