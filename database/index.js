c710ce84eca00de68f2769e9726a00f20469f079
Acacia Pappas <acacia.m.pappas@gmail.com>
2019-02-23 10:52:48 -0800

    reconfig .gitignore

--- database/index.js
+++ database/index.js
@@ -1,8 +1,8 @@
 const mysql = require('mysql');
 
 module.exports = mysql.createConnection({
-  // host: "172.17.0.2",
-  password: "yourpassword",
+  host: "172.17.0.2",
+  // password: "yourpassword",
   user: "root",
   database: "gallery"
 });
